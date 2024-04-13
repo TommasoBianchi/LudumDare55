1c940701dfb799c2455cf16150c884baf5809391
Tommaso Bianchi <tommaso.bianchi@polimi.it>
2024-04-13 19:05:57 +0200

    Change implementation of OrbitalMover

--- scripts/movements/orbital_movement.gd
+++ scripts/movements/orbital_movement.gd
@@ -5,7 +5,6 @@ class_name OrbitalMovement
 var _clockwise: bool
 var _radius: float
 var _angular_speed: float
-var _current_angle: float = -1
 
 func _init(clockwise: bool, radius: float, angular_speed: float):
 	self._clockwise = clockwise
@@ -21,11 +20,8 @@ func compute_next_direction(
 ) -> Vector2:
 	if not has_taget:
 		return Vector2.ZERO
-		
-	if _current_angle < 0:
-		_current_angle = (target_position - self_position).angle()
-		
-	var desired_position = target_position + Vector2.from_angle(_current_angle) * _radius
-	_current_angle += _angular_speed * delta * (1 if _clockwise else -1)
+
+	var current_angle = (self_position - target_position).angle()
+	var desired_position = target_position + Vector2.from_angle(current_angle + _angular_speed * delta * (1 if _clockwise else -1)) * _radius
 	
 	return (desired_position - self_position).normalized()
