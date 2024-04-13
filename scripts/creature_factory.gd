4aeef639b613e6231408d1f3cbf5f4d41d1e03ac
l-bianchi <illorenzobianchi@proton.me>
2024-04-13 18:57:20 +0200

    Passed properties to creature

--- scripts/creature_factory.gd
+++ scripts/creature_factory.gd
@@ -2,24 +2,22 @@ extends Node
 
 var creature_prefab: PackedScene = preload("res://scenes/creature.tscn")
 
-func _spawn_creature(position: Vector2, parent: Node, type: Creature.CreatureType) -> Creature:
-	var creature = creature_prefab.instantiate()
+func _spawn_creature(position: Vector2, parent: Node, type: Creature.CreatureType, stats: CreatureProperties.CreatureStats) -> Creature:
+	var creature: Creature = creature_prefab.instantiate()
 	creature.global_position = position
+	creature.movement = stats.movement
+	creature.targeter = stats.targeter
+	creature.move_speed = stats.speed
+	creature.current_health = stats.health
 	parent.add_child(creature)
 	return creature
 	
 func spawn_summon(position: Vector2, parent: Node, rune_level: int, summon_level: int):
-	var creature = _spawn_creature(position, parent, Creature.CreatureType.SUMMON)
 	var stats = CreatureProperties.get_summon_stats(rune_level, summon_level)
-	print("Summon name: ", stats.name)
-	print("Summon health: ", stats.health)
-	print("Summon damage: ", stats.damage)
+	var creature = _spawn_creature(position, parent, Creature.CreatureType.SUMMON, stats)
 	return creature
 	
 func spawn_enemy(position: Vector2, parent: Node):
-	var creature = _spawn_creature(position, parent, Creature.CreatureType.ENEMY)
 	var stats = CreatureProperties.get_enemy_stats("enemy_1")
-	print("Enemy name: ", stats.name)
-	print("Enemy health: ", stats.health)
-	print("Enemy damage: ", stats.damage)
+	var creature = _spawn_creature(position, parent, Creature.CreatureType.ENEMY, stats)
 	return creature
