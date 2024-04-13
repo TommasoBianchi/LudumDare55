db0514ae2acf32ae695afaf9c6fe78e3d0f55763
Tommaso Bianchi <tommaso.bianchi@polimi.it>
2024-04-13 16:36:51 +0200

    Fix walls collision code to make it accurate to detect walls collisions for ricochet behaviour

--- scripts/utils.gd
+++ scripts/utils.gd
@@ -5,13 +5,13 @@ enum WallPosition { UP, DOWN, LEFT, RIGHT }
 func find_colliding_walls(current_position: Vector2, map: Rect2) -> Array[WallPosition]:
 	var colliding_walls: Array[WallPosition] = []
 	
-	if current_position.x < map.position.x:
+	if current_position.x <= map.position.x:
 		colliding_walls.append(WallPosition.LEFT)
-	if current_position.x > map.end.x:
+	if current_position.x >= map.end.x:
 		colliding_walls.append(WallPosition.RIGHT)
-	if current_position.y < map.position.y:
+	if current_position.y <= map.position.y:
 		colliding_walls.append(WallPosition.UP)
-	if current_position.y > map.end.y:
+	if current_position.y >= map.end.y:
 		colliding_walls.append(WallPosition.DOWN)
 		
 	return colliding_walls
@@ -21,13 +21,13 @@ func keep_movement_in_map(current_position: Vector2, movement: Vector2, map: Rec
 	var colliding_walls = find_colliding_walls(next_position, map)
 	
 	if WallPosition.LEFT in colliding_walls:
-		movement.x = max(0, movement.x)
+		movement.x = max(map.position.x - current_position.x, movement.x)
 	if WallPosition.RIGHT in colliding_walls:
-		movement.x = min(0, movement.x)
+		movement.x = min(map.end.x - current_position.x, movement.x)
 	if WallPosition.UP in colliding_walls:
-		movement.y = max(0, movement.y)
+		movement.y = max(map.position.y - current_position.y, movement.y)
 	if WallPosition.DOWN in colliding_walls:
-		movement.y = min(0, movement.y)
+		movement.y = min(map.end.y - current_position.y, movement.y)
 		
 	return movement
 
