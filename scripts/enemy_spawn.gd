af5bac00486b50c45a6702a91358622fdf069365
Tommaso Bianchi <tommaso.bianchi@polimi.it>
2024-04-13 19:25:38 +0200

    Basic implementation of enemy spawnpoint

---
+++ scripts/enemy_spawn.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var spawn_delays: Array[int]
+@export var spawned_enemies_container: Node
+
+var _current_index: int = 0
+var _elapsed_time: float = 0
+
+func _process(delta):
+	_elapsed_time += delta
+	
+	var num_to_spawn = 0
+	while _current_index < len(spawn_delays) and _elapsed_time > spawn_delays[_current_index]:
+		_current_index += 1
+		num_to_spawn += 1
+		
+	_spawn_enemies(num_to_spawn)
+	
+	if _current_index >= len(spawn_delays):
+		_turn_off()
+		
+func _spawn_enemies(amount: int):
+	for i in range(amount):
+		var creature = CreatureFactory.spawn_enemy(global_position, spawned_enemies_container)
+	
+func _turn_off():
+	# TODO: add some VFX
+	queue_free()
