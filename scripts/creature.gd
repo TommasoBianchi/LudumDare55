fd159033265130d0a892c9e2f9cf24701f96cc42
Tommaso Bianchi <tommaso.bianchi@polimi.it>
2024-04-13 17:30:32 +0200

    Update creature script to integrate targeting

--- scripts/creature.gd
+++ scripts/creature.gd
@@ -10,19 +10,32 @@ var move_speed: float = 0
 var type: CreatureType
 var current_health: float
 var movement: BaseMovement = BaseMovement.new()
+var targeter: BaseTargeter = BaseTargeter.new()
+
+var _own_group = "summons" if type == CreatureType.SUMMON else "enemies"
+var _enemy_group = "enemies" if type == CreatureType.SUMMON else "summons"
 
 func _ready():
 	assert(animated_sprite != null)
+	add_to_group(_own_group)
 
 func _process(delta):
-	var enemy_creatures: Array[Creature] = []
-	var ally_creatures: Array[Creature] = []
+	var enemy_creatures: Array[Creature]
+	enemy_creatures.assign(get_tree().get_nodes_in_group(_enemy_group))
+	var ally_creatures: Array[Creature]
+	ally_creatures.assign(get_tree().get_nodes_in_group(_own_group))
 	
-	var direction = movement.compute_next_direction(
+	var target: Creature = targeter.compute_target(
 		global_position,
 		enemy_creatures,
 		ally_creatures,
-		Vector2.ZERO,  # player_position: Vector2,
+		get_tree().get_nodes_in_group("player")[0].global_position
+	)
+	var has_target: bool = target != null
+	var direction = movement.compute_next_direction(
+		global_position,
+		target.position if has_target else Vector2.ZERO,
+		has_target,
 		get_viewport_rect().grow(-50)
 	)
 	
