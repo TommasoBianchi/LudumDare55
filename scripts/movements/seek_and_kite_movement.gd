8fc84f4e1a40ae93801e9bb270e9aa17886581ce
Tommaso Bianchi <tommaso.bianchi@polimi.it>
2024-04-15 22:51:50 +0200

    Smooth SeekAndKiteMovement

--- scripts/movements/seek_and_kite_movement.gd
+++ scripts/movements/seek_and_kite_movement.gd
@@ -3,6 +3,8 @@ extends BaseMovement
 class_name SeekAndKiteMovement
 
 var _kite_distance: float
+var _change_direction_cooldown: float = 0
+var _is_too_close: bool = false
 
 func _init(kite_distance: float):
 	self._kite_distance = kite_distance
@@ -16,10 +18,13 @@ func compute_next_direction(
 ) -> Vector2:
 	if not has_taget:
 		return Vector2.ZERO
-		
-	var is_too_close = (target_position - self_position).length_squared() < _kite_distance ** 2
 
-	if is_too_close:
+	_change_direction_cooldown -= delta
+	if _change_direction_cooldown <= 0:
+		_is_too_close = (target_position - self_position).length_squared() < _kite_distance ** 2
+		_change_direction_cooldown = 0.75
+		
+	if _is_too_close:
 		return (self_position - target_position).normalized()
 	else:
 		return (target_position - self_position).normalized()
